0a7c2e7c1f49fddf29b32b37d137437b86a3bfa6
Leo Katzengruber <leofel.katz@gmail.com>
2023-12-02 11:22:21 +0100

    day 02 part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -10,7 +10,6 @@ enum Colour {
 }
 
 struct Game {
-    id: u32,
     pulls: Box<[[u32; Colour::AMOUNT_OF_COLOURS as usize]]>,
 }
 
@@ -18,10 +17,7 @@ impl TryFrom<&str> for Game {
     type Error = &'static str;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let (game, pulls) = value.split_once(": ").ok_or("invalid format")?;
-
-        let (_, game_id) = game.split_once(' ').ok_or("invalid format")?;
-        let game_id = game_id.parse().map_err(|_| "invalid format")?;
+        let (_, pulls) = value.split_once(": ").ok_or("invalid format")?;
 
         let pulls = pulls
             .split("; ")
@@ -51,32 +47,41 @@ impl TryFrom<&str> for Game {
             .collect::<Result<Vec<_>, &'static str>>()?;
 
         Ok(Self {
-            id: game_id,
             pulls: pulls.into_boxed_slice(),
         })
     }
 }
 
 impl Game {
-    fn is_plausible_with(
-        &self,
-        available_balls: &[u32; Colour::AMOUNT_OF_COLOURS as usize],
-    ) -> bool {
-        !self.pulls.iter().any(|pull| {
-            pull.iter()
-                .zip(available_balls.iter())
-                .any(|(pulled_balls, available_balls)| pulled_balls > available_balls)
-        })
+    fn calculate_power(&self) -> u64 {
+        let max_needed_balls = self.pulls.iter().fold(
+            [0; Colour::AMOUNT_OF_COLOURS as usize],
+            |mut max_needed_balls, needed_balls| {
+                for i in 0..Colour::AMOUNT_OF_COLOURS as usize {
+                    if max_needed_balls[i] < needed_balls[i] {
+                        max_needed_balls[i] = needed_balls[i];
+                    }
+                }
+
+                max_needed_balls
+            },
+        );
+
+        max_needed_balls
+            .iter()
+            .fold(1_u64, |acc, &curr| acc * curr as u64)
     }
 }
 
 fn main() {
-    let sum_of_ids = INPUT
+    let sum_of_powers = INPUT
         .lines()
-        .map(|line| Game::try_from(line).expect("invalid input"))
-        .filter(|game| game.is_plausible_with(&[12, 13, 14]))
-        .map(|game| game.id)
-        .sum::<u32>();
+        .map(|line| {
+            Game::try_from(line)
+                .expect("invalid input")
+                .calculate_power()
+        })
+        .sum::<u64>();
 
-    dbg!(sum_of_ids);
+    dbg!(sum_of_powers);
 }
