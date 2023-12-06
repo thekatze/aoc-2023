5d9f1f6d147f8f4edb4139288edecd0511db6b88
Leo Katzengruber <leofel.katz@gmail.com>
2023-12-06 15:19:12 +0100

    day 06 part 1

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,46 @@
+const INPUT: &str = include_str!("input.txt");
+// const INPUT: &str = "Time:      7  15   30
+// Distance:  9  40  200";
+
+struct Race {
+    time: u32,
+    record: u32,
+}
+
+impl Race {
+    fn count_record_time_configurations(&self) -> u32 {
+        (0..=self.time)
+            .map(|hold_time| {
+                let speed = hold_time;
+                let remaining_time = self.time - hold_time;
+
+                speed * remaining_time
+            })
+            .filter(|distance| distance > &self.record)
+            .count() as u32
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let parsed = INPUT
+        .lines()
+        .map(|line| {
+            let (_, numbers) = line.split_once(":").expect("invalid input");
+            numbers
+                .split_whitespace()
+                .map(|number| number.trim().parse::<u32>().expect("invalid input"))
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let times = parsed.get(0).expect("invalid input");
+    let records = parsed.get(1).expect("invalid input");
+
+    let result = times
+        .iter()
+        .zip(records.iter())
+        .map(|(&time, &record)| Race { time, record }.count_record_time_configurations())
+        .product::<u32>();
+
+    dbg!(result);
 }
